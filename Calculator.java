e77f1f4a43c7f7aec1685f9dac34bdb0bb0525f8
juwarabbani <rabbanijuwa@gmail.com>
2023-09-26 20:32:14 -0400

    programmer converter calculator

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Calculator {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        //continious loop that will display the program converter options
+        while (true) {
+            System.out.println("Programmer Converter Menu:");
+            System.out.println("1. Hex to Dec");
+            System.out.println("2. Dec to Hex");
+            System.out.println("3. Oct to Dec");
+            System.out.println("4. Dec to Oct");
+            System.out.println("5. Bin to Dec");
+            System.out.println("6. Dec to Bin");
+            System.out.println("7. Bitwise AND");
+            System.out.println("8. Bitwise OR");
+            System.out.println("9. Bitwise XOR");
+            System.out.println("0. Exit");
+    }
+}
+
+private static void hexToDec(Scanner scanner){
+    String hexString = scanner.nextLine();
+    }
+
+}
