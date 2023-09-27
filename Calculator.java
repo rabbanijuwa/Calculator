fc6c1e7e8ca5d7f5d7a91c733b360756b801ab2e
juwarabbani <rabbanijuwa@gmail.com>
2023-09-27 15:49:30 -0400

    programmer converter calculator

--- Calculator.java
+++ Calculator.java
@@ -18,11 +18,128 @@ public class Calculator {
             System.out.println("8. Bitwise OR");
             System.out.println("9. Bitwise XOR");
             System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    hexToDec(scanner);
+                    break;
+                case 2:
+                    decToHex(scanner);
+                    break;
+                case 3:
+                    octToDec(scanner);
+                    break;
+                case 4:
+                    decToOct(scanner);
+                    break;
+                case 5:
+                    binToDec(scanner);
+                    break;
+                case 6:
+                    decToBin(scanner);
+                    break;
+                case 7:
+                    bitwiseAnd(scanner);
+                    break;
+                case 8:
+                    bitwiseOr(scanner);
+                    break;
+                case 9:
+                    bitwiseXor(scanner);
+                    break;
+                case 0:
+                    System.out.println("Exiting the Programmer Converter. Goodbye!");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+        }
     }
+
+
+    private static void hexToDec(Scanner scanner) {
+        System.out.print("Enter a hexadecimal number: ");
+        String hexString = scanner.nextLine();
+        try {
+            int decimal = Integer.parseInt(hexString, 16);
+            System.out.println("Decimal: " + decimal);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid hexadecimal input.");
+        }
+    }
+
+    private static void decToHex(Scanner scanner) {
+        System.out.print("Enter a decimal number: ");
+        int decimal = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+        String hexString = Integer.toHexString(decimal);
+        System.out.println("Hexadecimal: " + hexString.toUpperCase());
+    }
+    private static void octToDec(Scanner scanner) {
+        System.out.print("Enter an octal number: ");
+        String octString = scanner.nextLine();
+        try {
+            int decimal = Integer.parseInt(octString, 8);
+            System.out.println("Decimal: " + decimal);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid octal input.");
+        }
+    }
+    private static void decToOct(Scanner scanner) {
+        System.out.print("Enter a decimal number: ");
+        int decimal = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+        String octString = Integer.toOctalString(decimal);
+        System.out.println("Octal: " + octString);
+    }
+    private static void binToDec(Scanner scanner) {
+        System.out.print("Enter a binary number: ");
+        String binString = scanner.nextLine();
+        try {
+            int decimal = Integer.parseInt(binString, 2);
+            System.out.println("Decimal: " + decimal);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid binary input.");
+        }
+    }
+    private static void decToBin(Scanner scanner) {
+        System.out.print("Enter a decimal number: ");
+        int decimal = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+        String binString = Integer.toBinaryString(decimal);
+        System.out.println("Binary: " + binString);
+    }
+    private static void bitwiseAnd(Scanner scanner) {
+        System.out.print("Enter the first decimal number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second decimal number: ");
+        int num2 = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+        int result = num1 & num2;
+        System.out.println("Bitwise AND Result: " + result);
+    }
+    private static void bitwiseOr(Scanner scanner) {
+        System.out.print("Enter the first decimal number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second decimal number: ");
+        int num2 = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+        int result = num1 | num2;
+        System.out.println("Bitwise OR Result: " + result);
+    }
+    private static void bitwiseXor(Scanner scanner) {
+        System.out.print("Enter the first decimal number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second decimal number: ");
+        int num2 = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+        int result = num1 ^ num2;
+        System.out.println("Bitwise XOR Result: " + result);
+    }
+
 }
 
-private static void hexToDec(Scanner scanner){
-    String hexString = scanner.nextLine();
-    }
-
-}
